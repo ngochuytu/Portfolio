ce4fbadb5761515d7a053b28206da35b5cdd4801
ngochuytu <tungochuy2001@gmail.com>
2025-12-07 22:19:37 +0700

    Fix: Projects not showing with small height screen

--- src/Components/Home/PageContainer/PageContainer.jsx
+++ src/Components/Home/PageContainer/PageContainer.jsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import Fade from 'react-reveal/Fade';
-import { Container } from './PageContainer.style';
+import React from "react";
+import Fade from "react-reveal/Fade";
+import { Container } from "./PageContainer.style";
 
 function PageContainer({ id, component, tagName }) {
-    return (
-        <Container id={id} as={tagName}>
-            <Fade>
-                {component}
-            </Fade>
-        </Container>
-    );
+  return (
+    <Container id={id} as={tagName}>
+      <Fade fraction={0.1}>{component}</Fade>
+    </Container>
+  );
 }
 
 export default PageContainer;
